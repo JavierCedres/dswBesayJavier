5647202e2724ccd5494d75c7ba3ad6509cd5e1c2
Besay GonzalezBenavides Garcia <besaygg@gmail.com>
2025-10-13 15:23:07 +0100

    Agregados los enlaces y plantillas

--- supertodo/tasks/views.py
+++ supertodo/tasks/views.py
@@ -4,9 +4,11 @@ from django.utils.text import slugify
 from .forms import AddTaskForm
 from .models import Task
 
+
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task/list.html', dict(tasks=tasks))
+
 
 def add_task(request):
     if (form := AddTaskForm(request.POST or None)).is_valid():
@@ -16,21 +18,27 @@ def add_task(request):
         return redirect('tasks:task-list')
     return render(request, 'tasks/task/add.html', dict(form=form))
 
+
 def task_detail(request, task_slug):
     task = Task.objects.get(slug=task_slug)
     return render(request, 'tasks/task/detail.html', dict(task=task))
-    
+
+
 def completed_tasks(request):
     return render(request, 'tasks/task/completed.html')
-    
+
+
 def pending_tasks(request):
-    return render(request, 'tasks/task/pending.html')
+    task_pending = Task.objects.filter(completed=False)
+    return render(request, 'tasks/task/pending.html', dict(tasks=task_pending))
+
 
 def toggle_status_task(request, task_slug):
     task = Task.objects.get(slug=task_slug)
     task.completed = not task.completed
     task.save()
-    return redirect('tasks:task-list')
+    return redirect(request.META['HTTP_REFERER'])
+
 
 def delete_task(request, task_slug):
     Task.objects.get(slug=task_slug).delete()
