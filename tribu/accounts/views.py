8bbe13dd237ebc965c3ae87ea9c069df2448ed7f
Besay GonzalezBenavides Garcia <besaygg@gmail.com>
2025-11-04 16:04:29 +0000

    urls

--- tribu/accounts/views.py
+++ tribu/accounts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-# Create your views here.
+from .forms import LoginForm, SignupForm
+from .models import Profile
+
+
+def user_login(request):
+    FALLBACK_REDIRECT = 'index'
+
+    if request.user.is_authenticated:
+        return redirect(reverse(FALLBACK_REDIRECT))
+    if request.method == 'POST':
+        if (form := LoginForm(request.POST)).is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            if user := authenticate(request, username=username, password=password):
+                login(request, user)
+
+                return redirect(request.GET.get('next', FALLBACK_REDIRECT))
+
+            else:
+                form.add_error(None, 'Incorrect username or password.')
+
+    else:
+        form = LoginForm()
+    return render(
+        request,
+        'accounts/login.html',
+        dict(form=form),
+    )
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+
+    return redirect('index')
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        if (form := SignupForm(request.POST)).is_valid():
+            user = form.save()
+            Profile.objects.create(bio='', user=user)
+
+            login(request, user)
+
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', dict(form=form))
+
+
+def profile(request):
+    return render({{request.user.profile}})
