7280c20836dd79fd06679405de48f37693d13d2c
B202XD <besaygg@gmail.com>
2026-01-06 23:23:54 +0000

    Signals

--- lumino/accounts/forms.py
+++ lumino/accounts/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from users.models import Profile
-
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -16,7 +14,7 @@ class SignupForm(forms.ModelForm):
         required = ('username', 'password', 'first_name', 'last_name', 'email')
         widgets = {'password': forms.PasswordInput}
         help_texts = {'username': None}
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -27,5 +25,4 @@ class SignupForm(forms.ModelForm):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password'])
         user = super().save(*args, **kwargs)
-        user.profile = Profile.objects.create(user=user)
         return user
